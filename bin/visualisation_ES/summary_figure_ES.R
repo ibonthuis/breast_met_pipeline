68351fb87dffaee7381f1bea1bc37716cccf13bc
ibonthuis <inebonthuis@gmail.com>
2026-01-14 14:31:27 +0100

    Removed absolute paths

--- bin/visualisation_ES/summary_figure_ES.R
+++ bin/visualisation_ES/summary_figure_ES.R
@@ -15,7 +15,7 @@ for (lib in required_libraries) {
   suppressPackageStartupMessages(library(lib, character.only = TRUE, quietly = TRUE))
 }
 
-vignette(package = "limma")
+#vignette(package = "limma")
 
 
 ## Functions
@@ -37,30 +37,7 @@ the_theme <- ggplot2::theme_set(
 )
 
 
-OUTPUT_DIR <- "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/results_summary_20251219"
 
-GSEA_COSGROVE <- "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_05112025/differential_indegrees/cosgrove/differential_genesets.tsv"
-GSEA_AURORA <- "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_05112025/differential_indegrees/aurora/differential_genesets.tsv"
-GSEA_COSGROVE_EXP <- "snakemake_results_expression_20251203/differential_indegrees/cosgrove/differential_genesets.tsv"
-GSEA_AURORA_EXP <- "snakemake_results_expression_20251203/differential_indegrees/aurora/differential_genesets.tsv"
-GSEA_EPITHELIAL <- "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_Epithelial_Myeloid_IFNpathway/differential_indegrees/Epithelial/differential_genesets.tsv"
-# GSEA_EPITHELIAL_EXP
-GSEA_T <- "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_T_Epithelial_IFNpathway/differential_indegrees/T/differential_genesets.tsv"
-GSEA_MYELOID <- "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_Epithelial_Myeloid_IFNpathway/differential_indegrees/Myeloid/differential_genesets.tsv"
-# GSEA_T_EXP
-# GSEA_MYELOID
-# GSEA_MYELOID_EXP
-
-# Define the file paths and their corresponding variable names
-file_paths <- list(
-  gsea_cosgrove = "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_05112025/differential_indegrees/cosgrove/differential_genesets.tsv",
-  gsea_aurora = "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_05112025/differential_indegrees/aurora/differential_genesets.tsv",
-  gsea_cosgrove_exp = "snakemake_results_expression_20251203/differential_indegrees/cosgrove/differential_genesets.tsv",
-  gsea_aurora_exp = "snakemake_results_expression_20251203/differential_indegrees/aurora/differential_genesets.tsv",
-  gsea_epithelial = "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_Epithelial_Myeloid_IFNpathway/differential_indegrees/Epithelial/differential_genesets.tsv",
-  gsea_t = "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_T_Epithelial_IFNpathway/differential_indegrees/T/differential_genesets.tsv",
-  gsea_myeloid = "/storage/kuijjerarea/ine/projects/BRCA_MET/breast_met_pipeline/snakemake_results_Epithelial_Myeloid_IFNpathway/differential_indegrees/Myeloid/differential_genesets.tsv"
-)
 
 # Read all files into data.tables and assign them to variables dynamically
 for (name in names(file_paths)) {
@@ -70,7 +47,69 @@ for (name in names(file_paths)) {
 # Check the loaded data.tables
 ls(pattern = "^gsea_")
 
+length(ls(pattern = "^gsea_"))
 
+rowname <- paste0(ls(pattern = "^gsea_")[1])
+df <- get(rowname)
+gsea_aurora_exp_rowname <- paste0(ls(pattern = "^gsea_")[2])
+gsea_aurora_exp <- gsea_aurora_exp[gsea_aurora_exp$pathway == "REACTOME_INTERFERON_SIGNALING", ]
+df <- df[df$pathway == "REACTOME_INTERFERON_SIGNALING", ]
+
+
+head(gsea_t)
+
+
+head(df)
+
+all_gsea <- data.frame(dataset_analysis = c(rowname, gsea_aurora_exp_rowname), 
+                        padj = c(df$PADJ_AUR1, gsea_aurora_exp$PADJ_AUR1),
+                        es = c(df$ES_AUR1, gsea_aurora_exp$ES_AUR1))
+head(all_gsea)
+
+for (i in 4:length(ls(pattern = "^gsea_"))) {
+  print(i)
+  rowname <- print(paste0(ls(pattern = "^gsea_")[i]))
+  df <- get(rowname)
+  if(nrow(df) > 1) {
+    df <- df[df$pathway == "REACTOME_INTERFERON_SIGNALING", ]
+  }
+  df$pathway <- rowname
+  row <- df[, c("pathway", "padj", "ES")]
+  colnames(row) <- c("dataset_analysis", "padj", "es")
+  all_gsea <- rbind(all_gsea, row)
+ # print(head(all_gsea))
+}
+
+head(all_gsea)
+all_gsea
+#regular_expression <- paste0("(FDR|PADJ).*", dataset, "|", dataset, ".*(FDR|PADJ)")
+
+all_gsea <- all_gsea %>%
+  mutate(sig = case_when(padj > 0.1 ~ "ns",
+                            TRUE ~ "significant"))
+
+all_gsea$dataset_analysis <- c("Validation", 
+                              "Validation expression", 
+                              "Discovery", 
+                              "Discovery expression",
+                              "Epithelial cells",
+                              "Epithelial cells expression",
+                              "Myeloid cells",
+                              "Myeloid cells expression",
+                              "T cells",
+                              "T cells expression")
+class(all_gsea$dataset_analysis)
+
+all_gse_plot <- ggplot(all_gsea, aes(x = dataset_analysis, y = es, fill = sig)) +
+                  geom_col()+
+                  coord_flip()+
+                  ylab("ES")+
+                  scale_fill_manual(values = c("grey60", "#377EB8"))+
+#                  labs(sig = "significant")+
+                  theme(axis.title.y = element_blank())
+all_gse_plot              
+
+standard_colors
 
 PSEUDOBULK_NORM <- "/storage/kuijjerarea/ine/projects/BRCA_SINGLECELL_TO_PSEUDOBULK/celltype_specific_pseudobulks/snakemake_results_myeloid/myeloid/pseudobulk_normalized.tsv"
 
